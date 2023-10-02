240020270d9d1c6b8fa15e11d30150ea3d0df833
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2023-10-02 16:16:44 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-gunicorn tortoolkit:start_server --bind 0.0.0.0:$PORT, --worker-class aiohttp.GunicornWebWorker
+gunicorn tortoolkit:start_server --bind 0.0.0.0:8000 --worker-class aiohttp.GunicornWebWorker & python3 -m tortoolkit
